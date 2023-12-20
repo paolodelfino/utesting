a05acc8675612f28c3c47f0fb30c89a11fbc6c74
paolodelfino <hackymail12@gmail.com>
2023-12-20 09:18:58 -0800

    Update docs

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslIKDUktLsnMS1fSUSpILMkAiiTnJBYXpxbrw2T0Mkpyc4DS2Zl5KUpWhkYWtbEAoSSvFD8AAAA="
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslIKSS0uiQ9OLVHSUSpILMkAiiTnJBYXpxbrw2T0Mkpyc4DS2Zl5KUpWhkYWtbEAhHVtQz8AAAA="
